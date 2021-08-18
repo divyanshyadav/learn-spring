d34e1e8f2850a10954f0944682299d5f43abd777
Divyansh <divyansh.yadav@gmail.com>
2021-08-18 16:12:49 +0530

    learn componentScan annotation

--- src/main/java/com/learn/spring/scope/ScopeApplication.java
+++ src/main/java/com/learn/spring/scope/ScopeApplication.java
@@ -5,10 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.learn.spring.basic.BinarySearchImpl;
 
 @SpringBootApplication
+@ComponentScan("com.learn.spring.basic") // search components in the given package
+@ComponentScan("com.learn.spring.scope")
 public class ScopeApplication {
 
 	private static Logger LOGGER = LoggerFactory.getLogger(ScopeApplication.class);
@@ -19,11 +22,10 @@ public class ScopeApplication {
 		
 		PersonDAO person1 = applicationContext.getBean(PersonDAO.class);
 		PersonDAO person2 = applicationContext.getBean(PersonDAO.class);
+		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
 		
-		LOGGER.info("Person1: {}", person1);
-		LOGGER.info("Person1 JDBC: {}", person1.getJdbcConnection());
-		
-		LOGGER.info("Person2: {}", person2);
-		LOGGER.info("Person2 JDBC: {}", person2.getJdbcConnection());
+		LOGGER.info("Person1: {} {}", person1, person1.getJdbcConnection());
+		LOGGER.info("Person2: {} {}", person2, person2.getJdbcConnection());
+		LOGGER.info("BinarySearch: {}", binarySearch);
 	}
 }
